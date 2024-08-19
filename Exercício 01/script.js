2e269343eb9aa22d761021e51517247180de6ea1
soraia-uchoa <soraiavercosa3@icloud.com>
2024-08-19 16:41:27 -0300

    Create script.js

---
+++ Exercício 01/script.js
@@ -0,0 +1,7 @@
+let nome = window.prompt("qual seu nome")
+let sobrenome = window.prompt("qual  seu sobrenome")
+let curso = window.prompt("qual seu curso")
+let ano = window.prompt("qual sua idade")
+let dataDeNascimento = 2024 - ano
+
+alert(`olá ${nome} ${sobrenome} então você estuda ${curso}, e tem ${dataDeNascimento} anos. legal!`)
