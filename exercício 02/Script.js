1db99efe20c0ef75d334cf5dc762275b7aac18fd
soraia-uchoa <soraiavercosa3@icloud.com>
2024-08-24 16:17:19 -0300

    Create Script.js

---
+++ exercício 02/Script.js
@@ -0,0 +1,13 @@
+let num1 = parseInt(prompt('digite o primeiro número'))
+let num2 = parseInt(prompt('digite o segundo número'))
+
+let soma = num1 + num2
+let sub = num1 - num2
+let mult = num1 * num2
+let div = num1 / num2
+
+alert(`Resultado:
+${num1} + ${num2} = ${soma}
+${num1} - ${num2} = ${sub}
+${num1} * ${num2} = ${mult} 
+${num1} / ${num2} = ${div} `)
